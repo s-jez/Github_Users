a19f28e73da5582787a21033b7bc84f96e2f6027
s-jez <stanislawjez7@gmail.com>
2022-07-14 21:04:35 +0200

    remove repo-search.js

--- js/user-search.js
+++ js/user-search.js
@@ -2,6 +2,7 @@ import { fetchData } from "./fetchData.js";
 const userInfo = document.querySelector(".user-info");
 const btnUserSearch = document.querySelector(".user-search");
 const apiURL = "https://api.github.com/users/";
+const repoURLs = [];
 
 btnUserSearch.addEventListener("click", () => {
   let userInput = document.getElementById("user-input").value;
@@ -9,6 +10,12 @@ btnUserSearch.addEventListener("click", () => {
   // Fetch User data
   fetchData(apiUserURL).then((data) => {
     showUserInfo(data.avatar_url, data.login);
+  });
+  const apiReposURL = apiURL + userInput + "/repos";
+  // Fetch User data
+  fetchData(apiReposURL).then((repo) => {
+    showRepoInfo(repo);
+    console.log(repo);
   });
 });
 
@@ -18,9 +25,27 @@ const showUserInfo = (urlImage, userText) => {
     return;
   }
   const userImage = document.createElement("img");
-  const userNick = document.createElement("p");
+  const userNick = document.createElement("h3");
   userImage.src = urlImage;
+  userImage.style.width = "55%";
   userNick.textContent = userText;
   userInfo.appendChild(userImage);
   userInfo.appendChild(userNick);
+};
+const showRepoInfo = (repo) => {
+  for (const el of repo) {
+    repoURLs.push(el.html_url);
+  }
+  console.log(repoURLs);
+  repoURLs.forEach((el) => {
+    const ul = document.createElement("ul");
+    const li = document.createElement("li");
+    const repoLinkElement = document.createElement("a");
+    ul.appendChild(li);
+    li.appendChild(repoLinkElement);
+    repoLinkElement.href = el;
+    repoLinkElement.textContent = el;
+    repoLinkElement.target = "_blank";
+    userInfo.appendChild(ul);
+  });
 };
