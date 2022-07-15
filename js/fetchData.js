77f8bfbd85cd855eeec2255c6c3567ed6f4aba4f
s-jez <stanislawjez7@gmail.com>
2022-07-15 18:29:42 +0200

    Add error check and count repositiories.

--- js/fetchData.js
+++ js/fetchData.js
@@ -1,7 +1,11 @@
 export const fetchData = async (url) => {
   const response = await fetch(url, {
-    Authorization: `token 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`,
+    Authorization: `token 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`,
     Acccept: `application/vnd.github+json`,
   });
+  if (!response.ok) {
+    const error = new Error("User not found!!!");
+    throw error;
+  }
   return await response.json();
 };
--- js/user-search.js
+++ js/user-search.js
@@ -1,51 +1,69 @@
 import { fetchData } from "./fetchData.js";
 const userInfo = document.querySelector(".user-info");
+const repoInfo = document.querySelector(".repo-info");
 const btnUserSearch = document.querySelector(".user-search");
+const errorMsg = document.querySelector(".error");
 const apiURL = "https://api.github.com/users/";
 const repoURLs = [];
 
 btnUserSearch.addEventListener("click", () => {
+  errorMsg.innerHTML = "";
   let userInput = document.getElementById("user-input").value;
   const apiUserURL = apiURL + userInput;
   // Fetch User data
-  fetchData(apiUserURL).then((data) => {
-    showUserInfo(data.avatar_url, data.login);
-  });
+  fetchData(apiUserURL)
+    .then((data) => {
+      console.log(data);
+      showUserInfo(data.avatar_url, data.login, data.html_url);
+    })
+    .catch((error) => {
+      if (error != null) {
+        errorMsg.textContent = error;
+      }
+    });
   const apiReposURL = apiURL + userInput + "/repos";
   // Fetch User data
   fetchData(apiReposURL).then((repo) => {
     showRepoInfo(repo);
-    console.log(repo);
   });
 });
 
-const showUserInfo = (urlImage, userText) => {
+const showUserInfo = (urlImage, userText, htmlURL) => {
   userInfo.innerHTML = "";
   if (urlImage === undefined || userText === undefined) {
     return;
   }
   const userImage = document.createElement("img");
+  const userProfile = document.createElement("a");
   const userNick = document.createElement("h3");
   userImage.src = urlImage;
   userImage.style.width = "55%";
-  userNick.textContent = userText;
+  userProfile.href = htmlURL;
+  userProfile.textContent = userText;
+  userProfile.target = "_blank";
+  userNick.appendChild(userProfile);
   userInfo.appendChild(userImage);
   userInfo.appendChild(userNick);
 };
 const showRepoInfo = (repo) => {
+  repoInfo.innerHTML = "";
   for (const el of repo) {
     repoURLs.push(el.html_url);
   }
-  console.log(repoURLs);
-  repoURLs.forEach((el) => {
-    const ul = document.createElement("ul");
+  let countRepos = repo.length;
+  const p = document.createElement("p");
+  const ul = document.createElement("ul");
+  repoURLs.forEach((el, index) => {
     const li = document.createElement("li");
     const repoLinkElement = document.createElement("a");
     ul.appendChild(li);
+    li.textContent = index + ": ";
     li.appendChild(repoLinkElement);
     repoLinkElement.href = el;
     repoLinkElement.textContent = el;
     repoLinkElement.target = "_blank";
-    userInfo.appendChild(ul);
   });
+  p.textContent = "Repositories: " + countRepos;
+  repoInfo.appendChild(p);
+  repoInfo.appendChild(ul);
 };
