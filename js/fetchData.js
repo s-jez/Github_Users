86cadfb417bfc63c83b263012910116242ba1cd2
s-jez <stanislawjez7@gmail.com>
2022-07-15 23:48:09 +0200

    Add HTML template file

--- js/fetchData.js
+++ js/fetchData.js
@@ -1,7 +1,9 @@
+const accessToken = "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";
+
 export const fetchData = async (url) => {
   const response = await fetch(url, {
-    Authorization: `token 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`,
     Acccept: `application/vnd.github+json`,
+    Authorization: "token " + accessToken,
   });
   if (!response.ok) {
     const error = new Error("User not found!!!");
