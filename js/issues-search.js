2b896bc522fe2284b9aa7d3b03f56e5e7c442b0a
s-jez <stanislawjez7@gmail.com>
2022-07-17 23:52:39 +0200

    Add issue raiting

--- js/issues-search.js
+++ js/issues-search.js
@@ -2,8 +2,32 @@ import { fetchData } from "./fetchData.js";
 
 const btnUserIssues = document.querySelector(".user-issues");
 const apiURL = "https://api.github.com/repos/";
-let issueRating = localStorage.getItem("rating");
 
+let issueRating = 0;
+
+const renderElements = (data) => {
+  for (let i in data) {
+    let ul = document.querySelector(".issues");
+    let li = document.createElement("li");
+    li.classList = "issue";
+    li.innerHTML = `
+    <p><strong>Issue Title: </strong>${data[i].title}</p>
+    <p><strong>User Login: </strong>${data[i].user.login}</p>
+    <p><strong>User Repository: </strong><a href='${data[i].html_url}' target='_blank'>${data[i].html_url}</a></p>
+    <p><strong>State:</strong>${data[i].state}</p>
+    <div class='raiting'>
+    <button class='plus' onclick='${issueRaitingIncrement}'>+</button>
+    <button class='minus' onclick='${issueRaitingDecrement}>-</button>
+    </div>
+    <br/>
+    `;
+    if (i === "10") {
+      return;
+    }
+    ul.appendChild(li);
+  }
+  issues.innerHTML = ul;
+};
 btnUserIssues.addEventListener("click", () => {
   let userInput = document.getElementById("user-input").value;
   const apiIssuesURL = apiURL + userInput + "/issues";
@@ -14,26 +38,13 @@ btnUserIssues.addEventListener("click", () => {
     })
     .catch((error) => console.log(error));
 });
-const renderElements = (data) => {
-  for (let i in data) {
-    let ul = document.querySelector(".issues");
-    let li = document.createElement("li");
-    li.classList = "issue";
-    li.innerHTML = `
-      <p><strong>Issue Title: </strong>${data[i].title}</p>
-      <p><strong>User Login: </strong>${data[i].user.login}</p>
-      <p><strong>User Repository: </strong><a href='${data[i].html_url}' target='_blank'>${data[i].html_url}</a></p>
-      <p><strong>State:</strong>${data[i].state}</p>
-      <div class='raiting'>
-        <div class='plus' onClick='setRaiting(${issueRating}, '+')'>+</div>
-        <div class='minus' onClick='setRaiting(${issueRating}, '-')'>-</div>
-      </div>
-      <br/>
-    `;
-    if (i === "10") {
-      return;
-    }
-    ul.appendChild(li);
-  }
-  issues.innerHTML = ul;
+const issueRaitingIncrement = () => {
+  issueRating++;
+  console.log(issueRating);
+  localStorage.setItem("issueRaiting", issueRating);
+};
+const issueRaitingDecrement = () => {
+  issueRating--;
+  console.log(issueRating);
+  localStorage.setItem("issueRaiting", issueRating);
 };
--- js/raiting.js
+++
@@ -1,8 +0,0 @@
-const setRaiting = (raiting, operation) => {
-  if (operation === "+") {
-    raiting++;
-  } else if (operation === "-") {
-    raiting--;
-  }
-  localStorage.setItem("rating", raiting);
-};
